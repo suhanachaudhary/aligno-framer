9bc6804e6ee8cc47641e2300bf971258dface695
CHAUDHARY_SUHANA <suhanachaudhary212@gmail.com>
2025-07-17 21:38:46 +0530

    animation added new

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -11,10 +11,12 @@ const SplashScreen = () => {
     return (
         <div className="min-h-screen bg-black relative overflow-hidden flex items-center justify-center">
             {/* Background shadow effects */}
-            <div className="absolute top-10% left-0 w-[700px] h-[600px] bg-[#0D5EA6] opacity-20 blur-[60px] z-0"></div>
-            <div className="absolute top-10% right-0 w-[600px] h-[600px] bg-[#0D5EA6] opacity-20 blur-[50px] z-0"></div>
+            <div className="absolute top-[30%] left-0 w-[700px] h-[130px] bg-[#093FB4] opacity-20 blur-[150px] z-0"></div>
+            <div className="absolute top-10% left-0 w-[700px] h-[600px] bg-[#093FB4] opacity-20 blur-[60px] z-0"></div>
+            <div className="absolute top-10% right-0 w-[600px] h-[600px] bg-[#093FB4] opacity-20 blur-[50px] z-0"></div>
             <div className="absolute inset-0 bg-gradient-to-br from-gray-900/30 via-black to-gray-800/20"></div>
             <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-gray-800/10 rounded-full blur-3xl"></div>
+            <div className="absolute top-[40%] right-0 w-[700px] h-[130px] bg-[#093FB4] opacity-20 blur-[150px] z-0"></div>
             <div className="absolute bottom-1/3 right-1/3 w-80 h-80 bg-gray-700/15 rounded-full blur-3xl"></div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[600px] h-[400px] bg-gradient-radial from-gray-800/20 to-transparent rounded-full blur-2xl"></div>
             <div className="text-center">
